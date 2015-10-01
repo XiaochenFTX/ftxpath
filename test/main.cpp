29b7458bc16ff109cea75b297bc6c48f86cff982
XiaochenFTX <xiaochenby706@sina.com>
2015-10-01 11:51:37 +0800

    add test

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 王晓辰 on 15/10/1.
+//
+
+#include <iostream>
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+    std::cout<< "1111111111 test ..." <<std::endl;
+    printf("222222222222222");
+
+    return 0;
+}
